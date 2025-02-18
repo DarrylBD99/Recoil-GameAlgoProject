1b02eff5d07e2a23db23374b1fb5a242cfbbd6b4
DarrylBD99 <darrylhoBD@gmail.com>
2025-02-18 15:52:02 +0800

    added damage flash function

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -1,7 +1,11 @@
+using System.Collections;
 using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
+    public delegate void IsDamaged();
+    public event IsDamaged OnDamaged;
+
     public float attack;
     public float speed;
     public float attackSpeed;
@@ -14,11 +18,6 @@ public class Entity : MonoBehaviour
         health = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update() {
-        
-    }
-
     // Damages Entity
     public void damage(Entity source) {
         if (source != null) {
@@ -27,13 +26,12 @@ public class Entity : MonoBehaviour
     }
 
     // Damages Entity
-    public void damage(float amount)
-    {
-        Debug.Log(name + ": Hit (" + health + "/" + maxHealth + ")");
+    public void damage(float amount) {
         health -= amount;
-        
-        if (health < 0)
-        {
+        Debug.Log(name + ": Hit (" + health + "/" + maxHealth + ")");
+        OnDamaged?.Invoke();
+
+        if (health <= 0) {
             Debug.Log(name + ": Dead");
             Destroy(gameObject);
         }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,24 @@
+using System.Collections;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     private float _speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    void Start() {
         Entity entity = GetComponent<Entity>();
         _speed = entity.speed;
+        entity.OnDamaged += DamageFlash;
     }
 
     // Update is called once per fixed frame
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         // Move player
         Vector3 velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         transform.position += velocity * Time.fixedDeltaTime * _speed;
+    }
+
+    void DamageFlash() {
+        GetComponent<SpriteRenderer>().color = Color.red;
     }
 }
