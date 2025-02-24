0c92c5cce69230bb4236060609226eb10ca89351
leeyep <leeyep325@gmail.com>
2025-02-24 15:44:10 +0800

    enemy spawners

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyPrefab;
+
+    [SerializeField]
+    private float minimumSpawnTime;
+
+    [SerializeField]
+    private float maximumSpawnTime;
+
+    private float spawnTime;
+    public Transform minSpawnPoint, maxSpawnPoint;
+
+    private void Awake()
+    {
+        ResetSpawnTimer();
+    }
+
+    void Update()
+    {
+        spawnTime -= Time.deltaTime;
+
+        if (spawnTime <= 0)
+        {
+            Instantiate(enemyPrefab, SelectSpawnPoint(), Quaternion.identity);
+            ResetSpawnTimer();
+        }
+    }
+
+    private void ResetSpawnTimer()
+    {
+        spawnTime = Random.Range(minimumSpawnTime, maximumSpawnTime);
+    }
+
+    public Vector3 SelectSpawnPoint()
+    {
+        Vector3 spawnPoint = Vector3.zero;
+        bool spawnVerticalEdge = Random.Range(0f, 1f) > 0.5;
+
+        if (spawnVerticalEdge)
+        {
+            spawnPoint.y = Random.Range(minSpawnPoint.position.y, maxSpawnPoint.position.y);
+
+            if (Random.Range(0f, 1f) > 0.5f)
+            {
+                spawnPoint.x = maxSpawnPoint.position.x;
+            }
+            else
+            {
+                spawnPoint.x = minSpawnPoint.position.x;
+            }
+        }
+        else
+        {
+            spawnPoint.x = Random.Range(minSpawnPoint.position.x, maxSpawnPoint.position.x);
+
+            if (Random.Range(0f, 1f) > 0.5f)
+            {
+                spawnPoint.y = maxSpawnPoint.position.y;
+            }
+            else
+            {
+                spawnPoint.y = minSpawnPoint.position.y;
+            }
+        }
+
+        return spawnPoint;
+    }
+}
