52497cc63e4821c3be838f3d5e18f89e116ea75e
DarrylBD99 <darrylhoBD@gmail.com>
2025-02-25 22:36:39 +0800

    F### UNITY'S INPUT MANAGER

--- Assets/Scripts/Entity/PlayerController.cs
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -12,38 +12,43 @@ public class PlayerController : MonoBehaviour
     private float _cooldown = 0f;
     private bool _isGrappleColliding = false;
 
+    // public InputActionReference attackAction; THE AMOUNT OF ISSUES THE NEW UNITY INPUT MANAGER GAVE ME IS UNBELIEVABLE
     public Transform bulletStart;
-    public InputActionReference attackAction;
     public static Entity PlayerInstance;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         _entity = GetComponent<Entity>();
         _sprite = gameObject.transform.Find("Sprite").gameObject;
-        attackAction.action.started += SpawnBullet;
 
         EnemyBase.Target = _entity;
         PlayerInstance = _entity;
     }
 
-    // Spawn bullet when attack action is pressed
-    void SpawnBullet(InputAction.CallbackContext context) {
-        if (PlayerInstance.IsUnityNull()) return;
 
-        if (!_isCooldown){
-            _entity.SpawnBullet(_mouseDir, bulletStart.position, _rotation);
-            _isCooldown = true;
-        }
-    }
+
+    // Spawn bullet when attack action is pressed
+    //void SpawnBullet(InputAction.CallbackContext context) {
+    //    if (PlayerInstance.IsUnityNull()) return;
+
+    //    if (!_isCooldown){
+    //        _entity.SpawnBullet(_mouseDir, bulletStart.position, _rotation);
+    //        _isCooldown = true;
+    //    }
+    //}
 
     // Update is called once per frame
     void Update() {
+        
         if (_isCooldown) {
             _cooldown += Time.deltaTime;
             if (_cooldown >= _entity.attackCooldown){
                 _isCooldown = false;
                 _cooldown = 0f;
             }
+        } else if (Input.GetMouseButtonDown(1)) {  // Spawn bullet when right click
+            _entity.SpawnBullet(_mouseDir, bulletStart.position, _rotation);
+            _isCooldown = true;
         }
     }
 
